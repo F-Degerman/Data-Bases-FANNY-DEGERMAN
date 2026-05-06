cfaff241bb303d57f3474ab5a162e6048cf74142
Fanny Degerman <fannydegerman@hotmail.com>
2026-05-06 15:39:32 +0200

    Fortsätter med labb1

--- labb_1.sql
+++ labb_1.sql
@@ -1,5 +1,8 @@
 USE everyloop;
 
+--- MOONMISSIONS
+SELECT TOP 10 * FROM MoonMissions;
+
 -- successful moon missions
 SELECT 
     Spacecraft,
@@ -13,6 +16,31 @@ WHERE Outcome = 'Successful';
 
 GO
 
-SELECT TOP 10 * FROM SuccessfulMissions;
+SELECT TOP 10 * 
+FROM SuccessfulMissions;
 
 GO
+
+--- Tar bort eventuella mellanslag i början eller slutet av Operator-kolumnen. 
+UPDATE SuccessfulMissions
+SET Operator = TRIM(Operator);
+
+GO
+
+--- DISTINCT för att få unika värden. Genom ORDER BY sorteras dessa alfabetiskt, 
+--- vilket gör att vi lätt kan se om mellanslag har tagits bort eller inte.
+SELECT DISTINCT Operator
+FROM SuccessfulMissions
+ORDER BY Operator; 
+
+GO
+
+UPDATE SuccessfulMissions
+SET Spacecraft = LEFT(Spacecraft, CHARINDEX('(', Spacecraft) - 1)
+WHERE Spacecraft LIKE '%(%';
+
+GO
+
+SELECT DISTINCT Spacecraft
+FROM SuccessfulMissions
+ORDER BY Spacecraft;
